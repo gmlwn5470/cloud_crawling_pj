cbc5260dfdc1915265fc11d0a8f9ef06f31cd947
gaebom2 <qkrgusdk110@naver.com>
2019-12-07 09:05:10 +0000

    글 model 정의

---
+++ django/firstproject/mysite/crawlingsite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrawlingsiteConfig(AppConfig):
+    name = 'crawlingsite'
---
+++ django/firstproject/mysite/crawlingsite/models.py
@@ -0,0 +1,16 @@
+frim django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    photo = models.ImageField(blank=True)
+   # created_date = models.DateTimeField(default=timezone.now)
+
+    def show(self):
+        self.save()
+        
+    def __str__(self):
+        return self.title
+
