dc09accdf205b27dce06acae578472fa373e9e4b
jayoung17 <young_1_5@naver.com>
2019-12-08 19:36:23 +0000

    html 수정

--- django/firstproject/mysite/crawlingsite/urls.py
+++ django/firstproject/mysite/crawlingsite/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('delivery_food.html', TemplateView.as_view(template_name="crawlingsite/delivery_food.html"), name='delivery_food'),
-    path('delivery_food', views.delivery_food, name='delivery_food'),
-    path('delivery_time', views.delivery_time, name='delivery_time'),
+    #path('delivery_food', views.delivery_food, name='delivery_food'),
+    path('delivery_time.html', TemplateView.as_view(template_name="crawlingsite/delivery_time.html"), name='delivery_time'),
     #url(r'^delivery_food', TemplateView.as_view(template_name="crawlingsite/delivery_food.html"), name='delivery_food')
 ]
