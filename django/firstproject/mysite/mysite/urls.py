3446a3d8cbba39f5bc1bfa0c0c683140efa9c0aa
gaebom2 <qkrgusdk110@naver.com>
2019-12-08 04:31:13 +0000

    html,css 파일 생성

--- django/firstproject/mysite/mysite/urls.py
+++ django/firstproject/mysite/mysite/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # url 파일 입니다.수정 ^^
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('crawlingsite.urls')),
 ]
