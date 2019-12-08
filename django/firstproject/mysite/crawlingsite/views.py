92cf8e15a22a9780bd893ba17413e9d13d8bb454
gaebom2 <qkrgusdk110@naver.com>
2019-12-09 06:20:51 +0900

    웹꾸몄음

--- django/firstproject/mysite/crawlingsite/views.py
+++ django/firstproject/mysite/crawlingsite/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_list(request):
-        return render(request, 'crawlingsite/post_list.html', {})
+from django.http import HttpResponse
+from django.template import loader
 
-def delivery_food(request):
-        return render(request, "crawlingsite/delivery_food.html", {})
 
-def delivery_time(request):
-        return render(request, "crawlingsite/delivery_time.html", {})
+def index(request):
+    template=loader.get_template('crawlingsite/index.html')
+    context={
+            'latest_question_list':"test",
+    }
+    return HttpResponse(template.render(context,request))
+
+#def post_list(request):
+#        return render(request, 'crawlingsite/post_list.html', {})
+
+#def delivery_food(request):
+#        return render(request, "crawlingsite/delivery_food.html", {})
+
+#def delivery_time(request):
+#        return render(request, "crawlingsite/delivery_time.html", {})
