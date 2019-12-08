d7af632f7452fd417fdc693605e3f1a7ecdb11c2
jayoung17 <young_1_5@naver.com>
2019-12-08 17:47:23 +0000

    웹페이지에  그래프 넣음

--- django/firstproject/mysite/crawlingsite/urls.py
+++ django/firstproject/mysite/crawlingsite/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('delivery_food.html', TemplateView.as_view(template_name="crawlingsite/delivery_food.html"), name='delivery_food'),
+    path('delivery_food', views.delivery_food, name='delivery_food'),
+    path('delivery_time', views.delivery_time, name='delivery_time'),
+    #url(r'^delivery_food', TemplateView.as_view(template_name="crawlingsite/delivery_food.html"), name='delivery_food')
 ]
--- django/firstproject/mysite/crawlingsite/views.py
+++ django/firstproject/mysite/crawlingsite/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def post_list(request):
-        return render(request, 'crawlingsite/post_list.html',{})
+        return render(request, 'crawlingsite/post_list.html', {})
+
+def delivery_food(request):
+        return render(request, "crawlingsite/delivery_food.html", {})
+
+def delivery_time(request):
+        return render(request, "crawlingsite/delivery_time.html", {})
